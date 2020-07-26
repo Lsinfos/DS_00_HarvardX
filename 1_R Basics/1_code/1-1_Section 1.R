a6c396ec0c19b7582c0528a65720523c18034618
ltrangng <tranglek1@hotmail.com>
2020-07-26 23:15:57 +0200

    the very basics

--- 1_R Basics/1_code/1-1_Section 1.R
+++ 1_R Basics/1_code/1-1_Section 1.R
@@ -1,6 +1,6 @@
 # SECTION 1: R BASICS, FUNCTIONS, AND DATA TYPES
 
-# 1.1 Getting started
+# 1.1 Getting started ####
 
 # R was developed by statisticians and data analysts as an interactive environment for data analysis.
 # Some of the advantages of R:
@@ -25,3 +25,51 @@ install.packages("tidyverse", "dslabs")
 # See all the installed packages
 installed.packages()
 # It is helpful to keep a list of all the needed packages in a script because a newer version of R doesn't re-install them automatically.
+
+# 1.2 The very basics ####
+
+# Object: stuffs that are stored in R. Variables could be defined and used to write expressions similar to maths.
+# E.g, solve the quadratic equation: x² + x - 1 = 0. Define variables to calculate the quadratic formular:
+a <- 1
+b <- 1
+c <- -1
+# To see the value stored in a variable, simply ask R to evaluate "a" and shows the stored value
+a
+# A more explicit way to ask R to show the valued store is using print
+print(a)
+
+# Workspace: as we define objects in the console, we are actually change the workspace. See all variables that are saved in the workspace:
+ls() # in Rstudio, the Environment tab shows the value.
+
+# Now since the values are saved in variables, obtain the solution to the equation by using the quadratic formula:
+(-b + sqrt(b^2 - 4*a*c))/(2*a)
+(-b - sqrt(b^2 - 4*a*c))/(2*a)
+
+# Functions: once variables are defined, the data analysis process can usually be described as a series of functions applied to the data.
+log(8)
+log(a) 
+# Argument: unlike ls(), most functions require one ore more inputs that called arguments. 
+# Find out what the function expects and what it does by reviewing R manuals
+help("log")
+?("log") # shortcut for most functions.
+# Have a quick look at the arguments without opening the manual
+args(log)
+# Change the default value by simply assigning another object:
+log(8, base = 2)
+log(8, 2) # if no argument name is used, R assumes entered arguments in the order shown in the help file.
+
+# prebuilt datasets: datasets that are included in Base R to practice and test out functions. View all the datasets:
+data() 
+# These datasets are objects that can be used by simply typing the name. E.g:
+co2 # Mauna Loa Atmospheric CO2 Concentration
+# Other prebuilt objects are mathematical quantities, such as the constant π and ∞:
+pi
+Inf + 1
+
+# Variable names: have to start with a letter, can’t contain spaces, and should not be variables that are predefined in R
+solution_1 <- (-b + sqrt(b^2 - 4*a*c))/(2*a)
+solution_2 <- (-b - sqrt(b^2 - 4*a*c))/(2*a)
+# Values remain in the workspace until the session is ended or being erase with rm()
+rm(solution_1)
+
+# 1.3 Case Study: US Gun Murders ####
