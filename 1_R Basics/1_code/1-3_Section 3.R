7470e4aeddf3d7f404bf1eb6fc97f8ab59e59d8c
ltrangng <tranglek1@hotmail.com>
2020-08-09 23:13:38 +0200

    Section 3

--- 1_R Basics/1_code/1-3_Section 3.R
+++ 1_R Basics/1_code/1-3_Section 3.R
@@ -37,3 +37,51 @@ c("Boston", "Dakota", "Washington") %in% murders$state
 # These two lines produce the same index, although in different order:
 match(c("New York", "Florida", "Texas"), murders$state)
 which(murders$state %in% c("New York", "Florida", "Texas"))
+
+# 3.2 Basic Data Wrangling ####
+
+# For more advanced analyses, we use dplyr package to manipulate data tables. Install and load dplyr
+install.packages("dplyr")
+library(dplyr)
+# Add the murder rate to the data frame with mutate() function
+murders <- mutate(murders, rate = total/population*100000) # "total" and "population" are not defined in the workspace, but mutate() knows to look for these variables in the data frame. 
+# Check the data frame again
+head(murders) # a new column "rate" is added.
+
+# filter(): takes the data table as the first argument and the conditional statement as the next. Filter the data table to only show the entries for which murder rate is lower than 0.71:
+filter(murders, rate <= 0.71)
+
+# select(): select just the columns you want to work with
+new_table <- select(murders, state, region, rate)
+# Filter the new table with entries that have murder rate lower than 0.71
+filter(new_table, rate <= 0.71) # first argument is an object.
+
+# Pipe operator (%>%): performs a series of operations in dplyr by sending the results of one function to another function
+murders %>% select(state, region, rate) %>% filter(rate <= 0.71) # same result as the previous code.
+
+# data.frame(): create a data frame. E.g,
+grades <- data.frame(names = c("John", "Juan", "Jean", "Yao"),
+                     exam_1 = c(95, 80, 90, 85),
+                     exam_2 = c(90, 85, 85, 90))
+
+# 3.3 Basic Plots ####
+
+# plot(): can be used to make a scattered plot. Make a plot of total murders vs. population
+x <- murders$population/10^6
+y <- murders$total
+plot(x, y)
+# For quick plot that avoids accessing variables twice, use with()
+with(murders, plot(population, total))
+
+# Create a histogram
+x <- with(murders, total/population*100000)
+hist(x) # the plot shows there's a wide range of values with most of them between 2 and 3 and one very extreme case with a murder rate of more than 15
+murders$state[which.max(x)] # "District of Columbia"
+
+# Create a boxplot
+murders$rate <- with(murders, total/population*100000)
+boxplot(rate~region, data = murders) # the plot shows the South has higher murder rate than other regions.
+
+# image(): displays the values in a matrix using color
+x <- matrix(1:120, 12, 10)
+image(x)
