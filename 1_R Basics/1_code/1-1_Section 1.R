cf8aa28de64684bc86c1708a49dbd89c2d03e0eb
Trang Ng <59824573+ltrangng@users.noreply.github.com>
2020-11-26 23:02:38 +0100

    refine section 1

--- 1_R Basics/1_code/1-1_Section 1.R
+++ 1_R Basics/1_code/1-1_Section 1.R
@@ -5,24 +5,24 @@
 # R was developed by statisticians and data analysts as an interactive environment for data analysis.
 # Some of the advantages of R:
 # (1) free and open source
-# (2) has the capacity to save scripts that could be run and shared across multiplatforms (Windows, Mac OS, Unix, Linux).
+# (2) has the capacity to save scripts that could be run and shared across multi-platforms (Windows, Mac OS, Unix, Linux).
 # (3) large and growing active R-user community, numerous resources for learning
-# (4) it is easy for developers to share software implementation
+# (4) it is easy for developers to share software implementation.
 
 # Interactive data analysis usually occurs on the R console that executes typed-in commands. E.g, calculate a 15% tip on a meal that costs $19.71
-19.71 * 0.15 # 2.96
+19.71 * 0.15
 
-# RStudio: an interactive, integrated development environment (IDE) is highly recommended to be used as a launching pad for most data science projects (see Cheatsheet RStudio in 03_resources). 
+# RStudio: an interactive, integrated development environment (IDE) is highly recommended to be used as a launching pad for most data science projects. 
 
 # "Base R" is what comes with the R installation. Additional components are available via packages. R makes it very easy to install packages, e.g, installing the dslabs package:
 install.packages("dslabs")
-# Once the package is installed, load it using liabry() function
+# Once the package is installed, load it using liabry() function.
 library(dslabs)
-# Install packages from RStudio interface: Tools>Install Packages (allows autocomplete)
-# Install more than one package by feeding a character vector
+# Install packages from RStudio interface: Tools>Install Packages (allows auto-complete)
+# Install more than one package by feeding a character vector:
 install.packages("tidyverse", "dslabs") 
 # The tidyverse package is actually several packages (dependencies). Dependencies can be controlled by adding the option "dependencies = TRUE".
-# See all the installed packages
+# See all the installed packages:
 installed.packages()
 # It is helpful to keep a list of all the needed packages in a script because a newer version of R doesn't re-install them automatically.
 
@@ -39,7 +39,7 @@ b <- 1
 c <- -1
 # To see the value stored in a variable, simply ask R to evaluate "a" and shows the stored value
 a
-# A more explicit way to ask R to show the valued store is using print
+# A more explicit way to ask R to show the valued store is using print.
 print(a)
 
 # Workspace: as we define objects in the console, we are actually change the workspace. See all variables that are saved in the workspace:
